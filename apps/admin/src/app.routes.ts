b1f107a886f6ae17d7db87f084986523ef97e324
fatmanurkaraman <fatmanur.karaman@outlook.com>
2025-09-24 21:18:19 +0300

    add dashboard page with layout routing

--- apps/admin/src/app.routes.ts
+++ apps/admin/src/app.routes.ts
@@ -3,6 +3,12 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
     {
         path:'',
-        loadComponent:()=> import('./pages/layouts/layouts')
+        loadComponent:()=> import('./pages/layouts/layouts'),
+        children:[
+            {
+                path:'',
+                loadComponent:()=> import('./pages/dashboard/dashboard')
+            }
+        ]
     }
 ];
